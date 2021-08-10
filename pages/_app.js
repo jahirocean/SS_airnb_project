3e2d2d6aa8411d4b53434d8669988cb8a708a548
Zahir Raihan <zahirraihann@gmail.com>
2021-08-10 17:04:37 +0600

    Day2 Build

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import "../styles/global.css";
+
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#fe595e",
+  className: "z-50",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("hashChangeComplete", progress.finish);
+Router.events.on("hashChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
